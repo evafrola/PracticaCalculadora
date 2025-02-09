e4578486eb2353a8309925a784622316cdbeab1a
Eva <eva_elizabeth_frola@hotmail.com>
2025-02-09 19:25:09 -0300

    cambios en la funcionalidad

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 //Variables
-const display = document.getElementById("display");
+const display = document.querySelector("display");
 const buttons = document.querySelectorAll("button");
 
 //Dandole funcionalidad a cada boton por un array de botones
 buttons.forEach((item) => {
-    item.onclick= ()=>{
+    item.addEventListener("click", ()=>{
       if(item.id === "limpiar"){  //Funcionalidad de el boton "C" que elimina todo
-        display.innerText="";
+        display.value="";
       } else if(item.id === "borrar"){ //Funcionalidad de el boton "<-" que borra letra por letra
-        let string = display.innerText.toString();
-        display.innerText = string.substring(0, string.length-1)
-      } else if(display.innerText != "" & item.id === "igual"){ //Funcionalidad de el boton "="
-
+        display.value = display.value.slice(0, -1)
+      } else if(display.value != "" && item.id === "igual"){ //Funcionalidad de el boton "="
+        display.value = eval(display.value)
       }
-    }
+    })
 });
